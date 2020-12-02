9a073f988d8420e91028f2e54bf874c668199bcf
Seth Etter <mail@sethetter.com>
2020-12-02 13:20:05 -0600

    Day 2, part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,7 +1,7 @@
 struct Rules<'a> {
     letter: &'a str,
-    min: usize,
-    max: usize,
+    x: usize,
+    y: usize,
 }
 
 fn main() {
@@ -11,10 +11,13 @@ fn main() {
         .filter(|line| {
             let parts: Vec<&str> = line.splitn(2, ":").collect();
             let (rules_str, pass) = (parts[0], parts[1]);
-
-            // split on :, then on space, then -
             let rules = parse_rules(rules_str);
-            check_pass(rules, pass)
+
+            // Part 1
+            // check_pass_1(rules, pass)
+
+            // Part 2
+            check_pass_2(rules, pass.trim())
         })
         .count();
 
@@ -26,12 +29,29 @@ fn parse_rules(input: &str) -> Rules {
     let (range, letter) = (parts[0], parts[1]);
 
     let range_parts: Vec<usize> = range.split("-").map(|p| p.parse().unwrap()).collect();
-    let (min, max) = (range_parts[0], range_parts[1]);
+    let (x, y) = (range_parts[0], range_parts[1]);
 
-    Rules { letter, min, max }
+    Rules { letter, x, y }
 }
 
-fn check_pass(rules: Rules, input: &str) -> bool {
+fn check_pass_1(rules: Rules, input: &str) -> bool {
     let c = input.to_owned().matches(rules.letter).count();
-    c >= rules.min && c <= rules.max
+    c >= rules.x && c <= rules.y
+}
+
+fn check_pass_2(rules: Rules, input: &str) -> bool {
+    // Get characters at the two indices specified by rules
+    let c1_opt = input.to_owned().chars().nth(rules.x - 1);
+    let c2_opt = input.to_owned().chars().nth(rules.y - 1);
+
+    // Ensure only 1 is a match
+    match (c1_opt, c2_opt) {
+        (Some(c1c), Some(c2c)) => {
+            let (c1, c2) = (c1c.to_string(), c2c.to_string());
+            (c1 == rules.letter && c2 != rules.letter) || (c2 == rules.letter && c1 != rules.letter)
+        }
+        (Some(c1c), None) => c1c.to_string() == rules.letter,
+        (None, Some(c2c)) => c2c.to_string() == rules.letter,
+        (None, None) => false,
+    }
 }
